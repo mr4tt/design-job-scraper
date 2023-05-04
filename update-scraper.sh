15ac17e1d2b75d700f9816984daba3fda42705b4
Mrat <78581772+mr4tt@users.noreply.github.com>
2023-05-04 16:58:25 -0700

    updating script

--- update-scraper.sh
+++ update-scraper.sh
@@ -4,9 +4,4 @@ now=$(TZ=America/Los_Angeles date +'%m/%d/%Y %H:%M %Z')
 
 python3 scrape-esdj.py
 
-git config --local user.email ${{ secrets.EMAIL }}
-git config --local user.name ${{ secrets.NAME }}
-
-git commit -m "adding new jobs on $now"
-
 git push 
