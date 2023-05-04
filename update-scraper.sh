1420756394b40b1b173a6ffb27c14e662e96b730
Mrat <78581772+mr4tt@users.noreply.github.com>
2023-05-04 14:43:27 -0700

    trying github actions

--- update-scraper.sh
+++ update-scraper.sh
@@ -2,10 +2,8 @@ git pull --no-edit
 
 now=$(TZ=America/Los_Angeles date +'%m/%d/%Y %H:%M %Z')
 
-cd /root/scrape-esdj
-
 python3 scrape-esdj.py
 
-git c "adding new jobs on $now"
+git commit -m "adding new jobs on $now"
 
 git push
